0765f2245e9aafc1b53693ceb8721ae4d9006690
Volgant <mr.volgant@mail.ru>
2021-02-22 17:31:17 +0300

    parametrize test

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,21 @@
+import pytest
+
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)
+@pytest.mark.parametrize('link', ["?promo=offer0",
+                                  "?promo=offer1",
+                                  "?promo=offer2",
+                                  "?promo=offer3",
+                                  "?promo=offer4",
+                                  "?promo=offer5",
+                                  "?promo=offer6",
+                                  pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                                  "?promo=offer8",
+                                  "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    original_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{0}".format(link)
+    page = ProductPage(browser, original_link)
     page.open()
     book_name, price = page.add_to_cart()
     page.solve_quiz_and_get_code()
